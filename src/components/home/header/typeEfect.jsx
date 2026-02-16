0db4c8fd1babecd29a391467b95205f8c4f5e690
smd0114 <pourtoi0114@outlook.com>
2026-02-15 22:24:48 -0500

    Overall Adjustments

--- src/components/home/header/typeEfect.jsx
+++ src/components/home/header/typeEfect.jsx
@@ -6,7 +6,7 @@ const TypewriterComponent = () => {
     <div className="notranslate mt-5 text-3xl font-bold">
       <Typewriter
         options={{
-          strings: ["Desarrollo Web", "Frontend", "Backend", "UX/UI"],
+          strings: ["Desarrollo Web", "Frontend", "Backend", "Ciberseguridad"],
           autoStart: true,
           loop: true,
         }}
