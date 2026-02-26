ff7630752fa3599894056d411594da7e1b0284d7
smd0114 <pourtoi0114@outlook.com>
2026-02-25 22:53:49 -0500

    Added Modelos

---
+++ projectData.js
@@ -0,0 +1,14 @@
+import VintageTone from "../assets/img-projects/vintageToneApp.jpg";
+
+
+export const proyectos = [
+  {
+    titulo: "Modelos E/R",
+    descripcion:
+      "Modelos Entidad-Relación en notación Chen",
+    imagen: VintageTone.src,
+    //tecnologias: ["Vue.js", "Tailwind", "Express", "MongoDB"],
+    codigo: "https://github.com/Lautaro-R-collins/VintageTone-Front-Vue.git",
+    
+  },
+];
